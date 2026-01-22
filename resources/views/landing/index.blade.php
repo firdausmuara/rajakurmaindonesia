cc54ef62c6fa2a38c5c34142d26ce800308b28ef
Muara Firdaus <muarafirdaus713@gmail.com>
2026-01-22 18:44:14 +0700

    update base color dan update menu dan update content

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -62,6 +62,51 @@
     </div>
 </section>
 
+<!-- Stats Section -->
+<section id="stats" class="stats section light-background">
+
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+        <div class="col-lg-3 col-md-6">
+            <div class="stats-item">
+            <i class="bi bi-emoji-smile"></i>
+            <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+            <p><strong>Happy Clients</strong> <span>consequuntur quae</span></p>
+            </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+            <div class="stats-item">
+            <i class="bi bi-journal-richtext"></i>
+            <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
+            <p><strong>Projects</strong> <span>adipisci atque cum quia aut</span></p>
+            </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+            <div class="stats-item">
+            <i class="bi bi-headset"></i>
+            <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
+            <p><strong>Hours Of Support</strong> <span>aut commodi quaerat</span></p>
+            </div>
+        </div><!-- End Stats Item -->
+
+        <div class="col-lg-3 col-md-6">
+            <div class="stats-item">
+            <i class="bi bi-people"></i>
+            <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
+            <p><strong>Hard Workers</strong> <span>rerum asperiores dolor</span></p>
+            </div>
+        </div><!-- End Stats Item -->
+
+        </div>
+
+    </div>
+
+</section><!-- /Stats Section -->
+
 <!-- Produk Section -->
 <section id="services" class="services section light-background">
     <div class="container section-title" data-aos="fade-up">
@@ -90,6 +135,68 @@
         </div>
     </div>
 </section>
+
+<!-- Call To Action Section -->
+<section id="call-to-action" class="call-to-action section dark-background">
+
+    <img src="{{ asset('landing/assets/img/cta-bg.jpg') }}" alt="">
+
+    <div class="container">
+        <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+        <div class="col-xl-10">
+            <div class="text-center">
+            <h3>Call To Action</h3>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a class="cta-btn" href="#">Call To Action</a>
+            </div>
+        </div>
+        </div>
+    </div>
+
+</section><!-- /Call To Action Section -->
+
+<!-- More Services Section -->
+<section id="more-services" class="more-services section">
+
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+        <h2>More Services</h2>
+        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+    </div><!-- End Section Title -->
+
+    <div class="container">
+
+        <div class="row gy-4">
+
+        <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="card">
+            <img src="{{ asset('landing/assets/img/more-service-1.jpg') }}" class="img-fluid" alt="">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
+            </div>
+        </div><!-- End Card Item -->
+
+        <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="card">
+            <img src="{{ asset('landing/assets/img/more-service-2.jpg') }}" class="img-fluid" alt="">
+            <h3>Voluptatem voluptatum alias</h3>
+            <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
+            </div>
+        </div><!-- End Card Item -->
+
+        <div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+            <img src="{{ asset('landing/assets/img/more-service-3.jpg') }}" class="img-fluid" alt="">
+            <h3>Fugit cupiditate alias nobis.</h3>
+            <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
+            </div>
+        </div><!-- End Card Item -->
+
+        </div>
+
+    </div>
+
+</section><!-- /More Services Section -->
 
 <!-- Berita Section -->
 <section id="portfolio" class="portfolio section">
@@ -125,6 +232,165 @@
         </div>
     </div>
 </section>
+
+<!-- Team Section -->
+<section id="team" class="team section">
+
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+        <h2>Team</h2>
+        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+    </div><!-- End Section Title -->
+
+    <div class="container">
+
+        <div class="row gy-4">
+
+            <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="member">
+                <img src="{{ asset('landing/assets/img/team/team-1.jpg') }}" class="img-fluid" alt="">
+                <div class="member-info">
+                    <div class="member-info-content">
+                    <h4>Walter White</h4>
+                    <span>Chief Executive Officer</span>
+                    <div class="social">
+                        <a href=""><i class="bi bi-twitter-x"></i></a>
+                        <a href=""><i class="bi bi-facebook"></i></a>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                <div class="member">
+                <img src="{{ asset('landing/assets/img/team/team-2.jpg') }}" class="img-fluid" alt="">
+                <div class="member-info">
+                    <div class="member-info-content">
+                    <h4>Sarah Jhonson</h4>
+                    <span>Product Manager</span>
+                    <div class="social">
+                        <a href=""><i class="bi bi-twitter-x"></i></a>
+                        <a href=""><i class="bi bi-facebook"></i></a>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="member">
+                <img src="{{ asset('landing/assets/img/team/team-3.jpg') }}" class="img-fluid" alt="">
+                <div class="member-info">
+                    <div class="member-info-content">
+                    <h4>William Anderson</h4>
+                    <span>CTO</span>
+                    <div class="social">
+                        <a href=""><i class="bi bi-twitter-x"></i></a>
+                        <a href=""><i class="bi bi-facebook"></i></a>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div><!-- End Team Member -->
+
+            <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="400">
+                <div class="member">
+                <img src="{{ asset('landing/assets/img/team/team-4.jpg') }}" class="img-fluid" alt="">
+                <div class="member-info">
+                    <div class="member-info-content">
+                    <h4>Amanda Jepson</h4>
+                    <span>Accountant</span>
+                    <div class="social">
+                        <a href=""><i class="bi bi-twitter-x"></i></a>
+                        <a href=""><i class="bi bi-facebook"></i></a>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div><!-- End Team Member -->
+
+        </div>
+
+    </div>
+
+</section><!-- /Team Section -->
+
+<!-- Faq Section -->
+<section id="faq" class="faq section light-background">
+
+    <div class="container">
+
+        <div class="row gy-4">
+
+            <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+                <div class="content px-xl-5">
+                <h3><span>Frequently Asked </span><strong>Questions</strong></h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                </p>
+                </div>
+            </div>
+
+            <div class="col-lg-8" data-aos="fade-up" data-aos-delay="200">
+
+                <div class="faq-container">
+                    <div class="faq-item faq-active">
+                        <h3><span class="num">1.</span> <span>Non consectetur a erat nam at lectus urna duis?</span></h3>
+                        <div class="faq-content">
+                        <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p>
+                        </div>
+                        <i class="faq-toggle bi bi-chevron-right"></i>
+                    </div><!-- End Faq item-->
+
+                    <div class="faq-item">
+                        <h3><span class="num">2.</span> <span>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</span></h3>
+                        <div class="faq-content">
+                        <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                        </div>
+                        <i class="faq-toggle bi bi-chevron-right"></i>
+                    </div><!-- End Faq item-->
+
+                    <div class="faq-item">
+                        <h3><span class="num">3.</span> <span>Dolor sit amet consectetur adipiscing elit pellentesque?</span></h3>
+                        <div class="faq-content">
+                        <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis</p>
+                        </div>
+                        <i class="faq-toggle bi bi-chevron-right"></i>
+                    </div><!-- End Faq item-->
+
+                    <div class="faq-item">
+                        <h3><span class="num">4.</span> <span>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</span></h3>
+                        <div class="faq-content">
+                        <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                        </div>
+                        <i class="faq-toggle bi bi-chevron-right"></i>
+                    </div><!-- End Faq item-->
+
+                    <div class="faq-item">
+                        <h3><span class="num">5.</span> <span>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</span></h3>
+                        <div class="faq-content">
+                        <p>Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in</p>
+                        </div>
+                        <i class="faq-toggle bi bi-chevron-right"></i>
+                    </div><!-- End Faq item-->
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</section><!-- /Faq Section -->
 
 <!-- Contact Section -->
 <section id="contact" class="contact section">
